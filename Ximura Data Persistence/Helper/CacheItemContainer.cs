30470bfcaaa251dfb6856a0a58f29ed598155b20
Paul Stancer <pstancer@gmail.com>
2010-02-24 07:36:52 +0000

    Small reorganization

--- Ximura Data Persistence/Helper/CacheItemContainer.cs
+++ Ximura Data Persistence/Helper/CacheItemContainer.cs
@@ -15,7 +15,6 @@ using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Runtime.Serialization;
