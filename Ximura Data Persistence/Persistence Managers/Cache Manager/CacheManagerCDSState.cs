1bd16874fa9cbd64820e0818dd0ba0457c7d9c06
Paul Stancer <pstancer@gmail.com>
2010-03-23 03:15:22 +0000

    Finished reorganization of the persistence layer to better match the new framework organization.

---
+++ Ximura Data Persistence/Persistence Managers/Cache Manager/CacheManagerCDSState.cs
@@ -0,0 +1,338 @@
+﻿#region Copyright
+// *******************************************************************************
+// Copyright (c) 2000-2009 Paul Stancer.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+//     Paul Stancer - initial implementation
+// *******************************************************************************
+#endregion
+#region using
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+
+using Ximura;
+using Ximura.Data;
+using Ximura.Helper;
+using CH = Ximura.Helper.Common;
+using RH = Ximura.Helper.Reflection;
+using AH = Ximura.Helper.AttributeHelper;
+using Ximura.Server;
+
+
+using Ximura.Command;
+#endregion // using
+namespace Ximura.Persistence
+{
+    /// <summary>
+    /// This is the cache manager PM state.
+    /// </summary>
+    [CDSStateActionPermit(CDSStateAction.Cache)]
+    [CDSStateActionPermit(CDSStateAction.Read)]
+    public class CacheManagerCDSState : FileSystemBasePMCDSState<Content, Content, CacheManagerCDSConfiguration>, ICDSCacheManager
+    {
+        #region Declarations
+        private ICDSCacheManagerBridge bridge = null;
+        /// <summary>
+        /// This is the file system storage manager.
+        /// </summary>
+        protected IXimuraStorageFileSystem storageFileSystem = null;
+        private object syncObject = new object();
+        #endregion // Declarations
+        #region Constructors
+        /// <summary>
+        /// This is the default constructor.
+        /// </summary>
+        public CacheManagerCDSState() : this(null) { }
+        /// <summary>
+        /// This is the component model constructor.
+        /// </summary>
+        /// <param name="container">The container</param>
+        public CacheManagerCDSState(IContainer container)
+            : base(container)
+        {
+        }
+        #endregion // Constructors
+
+        #region Read(CDSContext context)
+        /// <summary>
+        /// This is the read method which is used by cache managers to return the relevant content.
+        /// </summary>
+        /// <param name="context">The job context.</param>
+        protected override bool Read(CDSContext context)
+        {
+            if (!context.ContentIsCacheable)
+                return false;
+
+            Type objectType = context.Request.DataType;
+            //OK, get the unique identifier for the request.
+            ContentIdentifier? id;
+            if (!ProcessIdentifier(context, out id))
+                return false;
+            //If the request does not include a version ID then we cannot read from the cache
+            if (!context.Request.DataVersionID.HasValue)
+                return false;
+
+            Guid vid = context.Request.DataVersionID.Value;
+
+            CacheItemContainer<Content> cacheContainer = null;
+            lock (syncObject)
+            {
+                //Check whether we have the value in the cache.
+                if (!mDataCache.ContainsKey(id.Value))
+                    return false;
+                //Get the cache item.
+                cacheContainer = mDataCache[id.Value];
+                //If the cache item is valid, then clone the content and return.
+                if (!cacheContainer.CacheHasExpired)
+                {
+                    context.Response.Data = (Content)cacheContainer.ItemCloned(context.ContextSettings.PoolManager);
+                    context.Response.Status = CH.HTTPCodes.OK_200;
+                    return true;
+                }
+
+                //Ok, the cache has expired, but we can check whether the version is still valid and return the content.
+                Guid? CID, VID;
+                string vidStatus = context.CDSHelperDirect.Execute(objectType,
+                    CDSData.Get(CDSStateAction.VersionCheck, id.Value.ContentID, id.Value.VersionID), 
+                    out CID, out VID);
+                //Ok, the cached item is valid so we can return.
+                if (vidStatus == CH.HTTPCodes.OK_200 && VID.HasValue && id.Value.VersionID == VID.Value)
+                {
+                    cacheContainer.CacheRevalidate();
+
+                    context.Response.Data = (Content)cacheContainer.ItemCloned(context.ContextSettings.PoolManager);
+
+                    context.Response.Status = CH.HTTPCodes.OK_200;
+                    return true;
+                }
+
+                //So we cannot cache and need to remove the entity reference.
+                cacheContainer.CacheClear();
+                mDataCache.Remove(id.Value);
+                return false;
+            }
+        }
+        #endregion // Read(CDSContext context)
+        #region Cache(CDSContext context)
+        /// <summary>
+        /// This is the cache method which is used by cache managers to add new content to the cache.
+        /// </summary>
+        /// <param name="context">The job context.</param>
+        protected override bool Cache(CDSContext context)
+        {
+            if (!context.ContentIsCacheable)
+                return false;
+
+            CDSStateAction action = context.CDSStateActionResolve();
+
+            //Cache call can only support read at the moment.
+            switch (action)
+            {
+                case CDSStateAction.Read:
+                    break;
+                default:
+                    return false;
+            }
+
+            //Get the unique identifier for the context data object.
+            ContentIdentifier? id;
+            if (!ProcessIdentifier(context, out id))
+                return false;
+
+            lock (syncObject)
+            {
+                Content newContent = null;
+                try
+                {
+                    //Ok, nothing to do as the content is already in the cache.
+                    if (mDataCache.ContainsKey(id.Value))
+                        return false;
+
+                    newContent = (Content)context.Response.Data.Clone();
+
+                    mDataCache.Add(id.Value, new CacheItemContainer<Content>(newContent, true, 0));
+                }
+                catch (Exception ex)
+                {
+                    //ok, if there are any errors at this point we wipe out the cache value
+                    //and returns the objects to the pool.
+                    if (mDataCache.ContainsKey(id.Value))
+                        mDataCache.Remove(id.Value);
+
+                    if (newContent != null && newContent.ObjectPoolCanReturn)
+                    {
+                        newContent.ObjectPoolReturn();
+                        newContent = null;
+                    }
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        #endregion // Cache(CDSContext context)
+
+        #region SupportsEntityAction(CDSStateAction action, Type objectType)
+        /// <summary>
+        /// This method returns the 
+        /// </summary>
+        /// <param name="action">The actions.</param>
+        /// <param name="objectType">The object type.</param>
+        /// <returns>Returns int.max if the content type supports caching.</returns>
+        public override int SupportsEntityAction(CDSStateAction action, Type objectType)
+        {
+            //Only read and cache are supported.
+            if (action != CDSStateAction.Read && action != CDSStateAction.Cache)
+                return -1;
+
+            //If we support caching then we returns the highest priority 0, else we 
+            //returns -1 to signify that caching is not supported.
+            return (bridge.EntityTypeSupportsCaching(objectType))? 0 : -1;
+        }
+        #endregion // SupportsEntityAction(CDSStateAction action, Type objectType)
+
+        #region ServicesReference()
+        /// <summary>
+        /// This method registers the cache manager with the cachemanagerbridge service.
+        /// </summary>
+        protected override void ServicesReference()
+        {
+            base.ServicesReference();
+
+            bridge = GetService<ICDSCacheManagerBridge>();
+
+            if (bridge != null)
+            {
+                bridge.BridgeRegister(this);
+                bridge.PollRegister(this, 15);
+            }
+
+            storageFileSystem = GetService<IXimuraStorageFileSystem>();
+        }
+        #endregion // ServicesReference()
+        #region ServicesDereference()
+        /// <summary>
+        /// This method removes the cache manager from the bridge service.
+        /// </summary>
+        protected override void ServicesDereference()
+        {
+            storageFileSystem = null;
+
+            if (bridge != null)
+            {
+                bridge.BridgeUnregister(this);
+                bridge.PollUnregister(this);
+                bridge = null;
+            }
+
+            base.ServicesDereference();
+        }
+        #endregion // ServicesDereference()
+
+        #region CacheHits
+        /// <summary>
+        /// This text property contains a brief description of the number of cache hits for the cache collection.
+        /// </summary>
+        private string CacheHits
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+
+                mDataCache.Values
+                    .OrderBy(ci => ci.TotalHitCount)
+                    .ForEach(ci =>
+                        {
+                            sb.AppendFormat("{0} -> {1}", ci.TotalHitCount, ci.Identifier.ContentTypeAQNTruncated);
+                            sb.AppendLine();
+                        });
+
+                return sb.ToString();
+            }
+        }
+        #endregion // CacheHits
+        #region EntityCount
+        /// <summary>
+        /// This text property contains thebreakdown of cached entities in the cache manager.
+        /// </summary>
+        private string EntityCount
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                mDataCache.Values
+                    .GroupBy(ci => ci.Identifier.ContentTypeAQNTruncated)
+                    .OrderByDescending(g => g.Count())
+                    .ForEach(item =>
+                        {
+                            sb.AppendFormat("{0} -> {1}", item.Key, item.Count());
+                            sb.AppendLine();
+                        });
+
+                return sb.ToString();
+            }
+        }
+        #endregion // EntityCount
+
+        #region TimerPoll()
+        /// <summary>
+        /// This method is used to poll cache items that have aged.
+        /// </summary
+        public void TimerPoll()
+        {
+            try
+            {
+                List<KeyValuePair<ContentIdentifier, CacheItemContainer<Content>>> expired = null;
+
+                lock (syncObject)
+                {
+                    long size = mDataCache.Values.Sum(ci => ci.Size);
+
+                    //Ok, we will remove 10% if we have exceeded the maximum value.
+                    int number = mDataCache.Count / 10;
+
+                    if (number==0 || size < (134217728/2))
+                        return;
+
+                    expired = mDataCache
+                        .OrderByDescending(i => i.Value.ExpiryRating)
+                        .Take(number)
+                        .ToList();
+                }
+
+                foreach (var cacheItem in expired)
+                {
+                    //So we cannot cache and need to remove the entity reference.
+                    lock (syncObject)
+                    {
+                        if (mDataCache.ContainsKey(cacheItem.Key))
+                        {
+                            cacheItem.Value.CacheClear();
+                            mDataCache.Remove(cacheItem.Key);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ximura Data/CDSHelper/CDSHelper_Linq.cs
@@ -0,0 +1,211 @@
+﻿#region Copyright
+// *******************************************************************************
+// Copyright (c) 2000-2009 Paul Stancer.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+//     Paul Stancer - initial implementation
+// *******************************************************************************
+#endregion
+﻿#region using
+using System;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+using Ximura;
+using Ximura.Helper;
+using CH = Ximura.Helper.Common;
+using RH = Ximura.Helper.Reflection;
+using AH = Ximura.Helper.AttributeHelper;
+using Ximura.Data;
+using Ximura.Server;
+using Ximura.Command;
+
+
+#endregion // using
+namespace Ximura.Persistence
+{
+    public partial class CDSHelper 
+    {
+        /// <summary>
+        /// This property specifies the browse options
+        /// </summary>
+        [Flags]
+        public enum CDSBrowseConstraints : int
+        {
+            /// <summary>
+            /// The CDS will browse on all content fields. This may be very slow.
+            /// </summary>
+            All = 0,
+            /// <summary>
+            /// The CDS will only browse of fields that have been marked relational.
+            /// </summary>
+            Relational = 1
+        }
+
+        public class BrowseContext<T> : IQueryable<T>, IQueryProvider
+            where T : Content
+        {
+            #region Declarations
+            private IXimuraSessionRQ mSessionRQ;
+            private Expression mExpression = null;
+            private IList<T> results;
+            private CDSBrowseConstraints mConstraints;
+            #endregion // Declarations
+            #region Constructor
+            /// <summary>
+            /// This is the default constructor for the browse context.
+            /// </summary>
+            /// <param name="SessionRQ">The session object to execute under.</param>
+            /// <param name="constraints">The browse constraints identifying what sort of request should be accepted.</param>
+            public BrowseContext(IXimuraSessionRQ SessionRQ, CDSBrowseConstraints constraints)
+            {
+                mSessionRQ = SessionRQ;
+                mConstraints = constraints;
+            }
+            #endregion // Constructor
+
+            #region IQueryProvider Members
+
+            public IQueryable<S> CreateQuery<S>(Expression expression)
+            {
+                if (typeof(S) != typeof(T))
+                    throw new Exception("Only " + typeof(T).FullName + " objects are supported.");
+
+                this.mExpression = expression;
+
+                return (IQueryable<S>)this;
+            }
+
+            public IQueryable CreateQuery(Expression expression)
+            {
+                return (IQueryable<T>)(this as IQueryProvider).CreateQuery<T>(expression);
+            }
+
+            public TResult Execute<TResult>(Expression expression)
+            {
+                MethodCallExpression methodcall = mExpression as MethodCallExpression;
+
+                foreach (var param in methodcall.Arguments)
+                {
+                    ProcessExpression(param);
+                }
+
+                return (TResult)results.GetEnumerator();
+            }
+
+            public object Execute(Expression expression)
+            {
+                return (this as IQueryProvider).Execute<IEnumerator<T>>(expression);
+            }
+
+            #endregion
+
+            #region IEnumerable<T> Members
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return (this as IQueryable).Provider.Execute<IEnumerator<T>>(mExpression);
+            }
+
+            #endregion
+
+            #region IEnumerable Members
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return (IEnumerator<T>)(this as IQueryable).GetEnumerator();
+            }
+
+            #endregion
+
+            #region IQueryable Members
+
+            public Type ElementType
+            {
+                get { return typeof(T); }
+            }
+
+            public Expression Expression
+            {
+                get { return Expression.Constant(this); }
+            }
+
+            public IQueryProvider Provider
+            {
+                get { return this; }
+            }
+
+            #endregion
+
+            private void ProcessExpression(Expression expression)
+            {
+                //RQRSContract<CDSRequestFolder, CDSResponseFolder> rqEnv = null;
+                //Content rsData = null;
+                //try
+                //{
+                //    rqEnv = EnvelopeRequest(
+                //        new EnvelopeAddress(scContentDataStore.CDSCommandID, ));
+                //    //Get the request
+                //    CDSRequestFolder rq = rqEnv.ContractRequest;
+                //    //Set the value type
+                //    rq.DataType = itemType;
+                //    rq.ByReference = rqH.ByReference;
+                //    rq.DataReferenceType = rqH.RefType;
+                //    rq.DataReferenceValue = rqH.RefValue;
+
+                //    if (rqH.IDContent.HasValue)
+                //        rq.DataContentID = rqH.IDContent.Value;
+                //    if (rqH.IDVersion.HasValue)
+                //        rq.DataVersionID = rqH.IDVersion.Value;
+
+                //    rq.Data = null;
+
+                //    SessionRQ.ProcessRequest(rqEnv, rqH.Priority);
+
+                //    CDSResponseFolder rs = rqEnv.ContractResponse;
+                //    if (rqEnv.ContractRequest.InternalCall &&
+                //        rqH.Action == CDSStateAction.ResolveReference)
+                //    {
+                //        cid = rq.DataContentID;
+                //        vid = rq.DataVersionID;
+                //    }
+                //    else
+                //    {
+                //        cid = rs.CurrentContentID;
+                //        vid = rs.CurrentVersionID;
+                //    }
+
+                //    rsData = rs.Data;
+
+                //    return rs.Status;
+                //}
+                //catch (Exception ex)
+                //{
+                //    throw ex;
+                //}
+                //finally
+                //{
+                //    if (rsData != null && rsData.ObjectPoolCanReturn)
+                //        rsData.ObjectPoolReturn();
+                //    rqH.ObjectPoolReturn();
+                //    EnvelopeReturn(rqEnv);
+                //}
+            }
+
+
+        }
+    }
+}
